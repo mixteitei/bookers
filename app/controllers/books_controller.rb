b89906964d80375d2661eca717361eb65359d124
mixteitei <jittoshiteruyo@gmail.com>
2021-10-15 13:15:26 +0000

    [add_create]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,23 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
   end
 
   def show
   end
 
-  def new
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to'/'
   end
 
   def edit
   end
+end
+
+private
+def book_params
+  params.require(:book).permit(:title, :body)
 end
