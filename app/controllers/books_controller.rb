462565f0a73b73b44dee8c6ef54ae50fa1d1f99b
mixteitei <jittoshiteruyo@gmail.com>
2021-10-16 08:43:00 +0000

    [add_index]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,32 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
     @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to'/'
+    redirect_to book_path(book.id)
   end
 
   def edit
   end
-end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
-private
-def book_params
-  params.require(:book).permit(:title, :body)
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
